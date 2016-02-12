85f331da7a6ac61ad6ccd607ac56bbc915810326
Richard Ciampa <rciampa@csumb.edu>
2016-02-11 22:05:14 -0800

    Updated to use hex values from assignment

--- clab/gScale.c
+++ clab/gScale.c
@@ -3,26 +3,41 @@
 void showbits(unsigned int x)
 {
    int i; 
-   for(i=(sizeof(int)*8)-1; i>=0; i--)
-   (x&(1<<i))?printf("1"):printf("0");
+   //Because int is 4 bytes we multiply by 8 (8 bits/byte)
+   for(i = (sizeof(int) * 8) - 1; i >= 0; i--)
+   (x&(1 << i))? printf("1") : printf("0");
     
     printf("\n");
 }
 
+void convert_to_grayscale(unsigned int pixelarr[], unsigned int size){
+    printf("pix 1: %u pix 2: %u \n", pixelarr[0], pixelarr[1]);
+}
+
 int main(int argc, char *argv) 
 {
-   int j = 5225, m, n;
-   printf("\nThe decimal %d is equal to binary - ", j);
+
+  int j = 5225, m, n, size;
+  //Two int values as hex A R G B
+  unsigned int pixels [] = {0xAA112233, 0xBB334455};
+  size = 2;
+
+  convert_to_grayscale(pixels, 2);
+  
    /* assume we have a function that prints a binary string when given 
     * a decimal integer 
     */
-   showbits(j);
+   int p;
+   for(p = 0; p < size; p++){
+      printf("\nThe decimal %u is equal to binary - ", pixels[p]);
+      showbits(pixels[p]);
 
-   /* the loop for right shift operation */
-   for ( m = 0; m <= 5; m++ ) {
-      n = j >> m;
-      printf("%d right shift %d gives ", j, m);
-      showbits(n);
+      /* the loop for right shift operation */
+      for ( m = 0; m <= 5; m++ ) {
+         n = pixels[p] >> m;
+         printf("%u right shift %d gives ", pixels[p], m);
+         showbits(n);
+      }
    }
 
   printf("\n");

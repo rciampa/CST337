8bda59c9f60fac8c9e0de23ecca22b2986431231
Richard Ciampa <rciampa@csumb.edu>
2016-02-19 00:09:17 -0800

    Homework 5 problem 1 sample

---
+++ hw5/hw51.c
@@ -0,0 +1,36 @@
+/*
+ * Author: Richard Ciamps
+ * Course: CST337
+ * Date: 2/18/2016
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+char and_gate(char a, char b);
+
+int main(int argc, char *argv[]){
+ 
+ if(argc == 3 && (strlen(argv[1]) == strlen(argv[2]))){
+  
+  //We only need the size of one because they are equal   
+  size_t argSize = strlen(argv[1]);
+  char result[argSize];
+  //Loop through the command line args 
+  for(int i = 0; i < argSize; i++){
+   result[i] = and_gate(argv[1][i], argv[2][i]);
+  }
+  //Print the result
+  printf("Result of AND: %s \n", result);
+  
+ }
+  return 0;
+}
+
+char and_gate(char a, char b){ 
+   if(a == '0' || b == '0'){
+     return '0';
+   }else{
+     return '1';
+   }
+}

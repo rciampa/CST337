17177e98657f69b83ab6e49f23d2ddb69168d43a
Richard Ciampa <rciampa@csumb.edu>
2016-02-20 00:23:03 -0800

    Changed printf() to puts() for new line

--- hw5/hw52.c
+++ hw5/hw52.c
@@ -1,5 +1,5 @@
 /*
- * Author: Richard Ciampa
+ * Author: Richard Ciamps
  * Course: CST337
  * Date: 2/18/2016
  */
@@ -7,27 +7,26 @@
 #include <stdio.h>
 #include <string.h>
 
-char and_gate(char a, char b);
+char or_gate(char a, char b);
 
 int main(int argc, char *argv[]){
  
  if(argc == 3 && (strlen(argv[1]) == strlen(argv[2]))){
-  //We only need the size of one because they are equal   
+    //We only need the size of one because they are equal   
   size_t argSize = strlen(argv[1]);
   //Print the result
-  printf("Result of AND: ");
+  printf("Result of OR: "); 
   //Loop through the command line args 
   for(int i = 0; i < argSize; i++){
-  //Print the result
-  printf("%c",and_gate(argv[1][i], argv[2][i]));
+    printf("%c", or_gate(argv[1][i], argv[2][i]));
   }
   puts("\n");
  }
   return 0;
 }
 
-char and_gate(char a, char b){ 
-   if(a == '0' || b == '0'){
+char or_gate(char a, char b){ 
+   if(a == '0' && b == '0'){
      return '0';
    }else{
      return '1';
